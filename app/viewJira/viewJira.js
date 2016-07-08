edb1ad8c0543d708997dddd900063d4cb545f0a8
imjakeayala <jakeayala@gmail.com>
2016-07-08 14:10:38 -0700

    add jira and meeting view content

--- app/viewJira/viewJira.js
+++ app/viewJira/viewJira.js
@@ -9,6 +9,18 @@ angular.module('myApp.viewJira', ['ngRoute'])
   });
 }])
 
-.controller('ViewJiraCtrl', [function() {
+.controller('ViewJiraCtrl', ['$scope', function($scope) {
+  $scope.checkIfResultsReady = function() {
+    $scope.resultsReady = ($scope.issueType === 'story' && $scope.role && $scope.outcome && $scope.benefit && $scope.acceptanceCriteria) ||
+                          ($scope.issueType === 'bug' && $scope.repro && $scope.actual && $scope.expected) ||
+                          ($scope.issueType === 'task' && $scope.requirements);
+    console.log($scope.resultsReady)
+  };
+
+  $scope.setIssueType = function(type) {
+    $scope.issueType = type;
+    console.info($scope.issueType);
+    console.info($scope.resultsReady);
+  }
 
 }]);
--- app/viewMeeting/viewMeeting.js
+++ app/viewMeeting/viewMeeting.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.viewMeeting', ['ngRoute'])
+angular.module('myApp.viewMeeting', ['ngRoute', 'ngCookies'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/viewMeeting', {
@@ -9,6 +9,42 @@ angular.module('myApp.viewMeeting', ['ngRoute'])
   });
 }])
 
-.controller('ViewMeetingCtrl', [function() {
+.controller('ViewMeetingCtrl', ['$scope', '$cookies', function($scope, $cookies) {
+  $scope.bg = 'Knausgaard austin swag PBR&B fashion axe, sriracha pug fixie. Locavore twee tilde migas, normcore irony blog paleo poutine man braid. Everyday carry mlkshk PBR&B skateboard, try-hard fingerstache forage disrupt thundercats. Roof party raw denim vinyl hella normcore messenger bag, mlkshk crucifix vegan direct trade kinfolk tofu.';
+  $scope.agenda = '1. Excepteur plaid whatever cliche\n';
+  $scope.agenda += '2. Veniam salvia taxidermy';
+  $scope.appendix = 'See http://x.co/practices'
 
+  // Use cookies to save links
+  $scope.saveZoomLink = function(){
+    if ($scope.zoomName) {
+      $cookies.put('zoomName', $scope.zoomName);
+      console.info('saved zoom name')
+    }
+    if ($scope.zoomNumber) {
+      $cookies.put('zoomNumber', $scope.zoomNumber);
+      console.info('saved zoom number')
+    }
+    console.info('cookies attempt done');
+    console.info($cookies);
+  };
+
+  $scope.clearCookies = function(){
+    $cookies.remove('zoomName');
+    $scope.zoomName = null;
+    $cookies.remove('zoomNumber');
+    $scope.zoomNumber = null;
+    console.info('cookies cleared');
+  };
+
+  // Retrieve links from cookies
+  var zoomName = $cookies.get('zoomName');
+  if (zoomName) {
+    $scope.zoomName = zoomName;
+  }
+
+  var zoomNumber = $cookies.get('zoomNumber');
+  if (zoomNumber) {
+    $scope.zoomNumber = zoomNumber;
+  }
 }]);
